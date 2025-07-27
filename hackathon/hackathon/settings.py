1f0d852ab02dfe6b6fd11adf965902301c44f112
parsamhb82 <parsa1382mohebian@gmail.com>
2025-07-27 22:09:49 +0330

    fixed settings

--- hackathon/hackathon/settings.py
+++ hackathon/hackathon/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "user",
+    "rest_framework",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
--- hackathon/user/urls.py
+++ hackathon/user/urls.py
@@ -4,5 +4,5 @@ from user.views import UserLoginView, UserRefreshView, UserCreateView
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('refresh/', UserRefreshView.as_view(), name='user-refresh'),
-    path('create/', UserCreateView.as_view(), name='user-create'),
+    path('register/', UserCreateView.as_view(), name='user-register'),
 ]
