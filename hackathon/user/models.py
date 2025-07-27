e0dec49d0272fc938a18cb202a217a680c86d369
parsamhb82 <parsa1382mohebian@gmail.com>
2025-07-27 20:48:01 +0330

    user model made

--- hackathon/user/models.py
+++ hackathon/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    bio = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=15)
+    profile_picture = models.ImageField(upload_to="profile_pictures/", blank=True)
+
+    def __str__(self):
+        return self.user.username
